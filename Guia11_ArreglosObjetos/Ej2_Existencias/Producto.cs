0000ab1d3c3dd0130ea7488d904ee80cae53cd09
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-05-29 06:58:54 -0300

    iniciando

---
+++ Guia11_ArreglosObjetos/Ej2_Existencias/Producto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej2_Existencias
+{
+    class Producto
+    {
+        static int GenNum;
+        public int Codigo { get;private set; }
+        public int Cantidad { get; private set; }
+        public string Nombre { get;private  set; }
+
+        public Producto(string nombre, int cant) {
+            Nombre = nombre;
+            Cantidad = cant;
+        }
+
+        public int AgregarCantidad(int reposicion) {
+            return Cantidad += reposicion;
+        }
+
+        public int QuitarCantidad(int extraccion) {
+            return Cantidad -= extraccion;
+        }
+    }
+}
---
+++ Guia11_ArreglosObjetos/Ej2_Existencias/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej2_Existencias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
